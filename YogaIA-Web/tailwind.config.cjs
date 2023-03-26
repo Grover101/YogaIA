72574c81574b22f1626d5ddd0c533230167cf49a
Grover101 <limachigrover09@gmail.com>
2023-03-26 17:28:26 -0400

    config color required

--- YogaIA-Web/tailwind.config.cjs
+++ YogaIA-Web/tailwind.config.cjs
@@ -2,6 +2,13 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            white: '#ffffff',
+            orangeColor: '#EF6F13',
+            blckGray: '#242424'
+        },
         extend: {}
     },
     plugins: []
