288e97704d3491e0236e55eff8fbb8d93111b10b
Grover101 <limachigrover09@gmail.com>
2023-03-31 23:53:52 -0400

    config and connect mongodb

--- YogaIA-BackEnd/app.js
+++ YogaIA-BackEnd/app.js
@@ -2,10 +2,19 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const { connect } = require('./app/models')
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+connect()
+    .then(() => {
+        console.log('DB is connected to mongo')
+    })
+    .catch(error => {
+        console.error('Database connection error', error)
+    })
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to bezkoder application.' })
