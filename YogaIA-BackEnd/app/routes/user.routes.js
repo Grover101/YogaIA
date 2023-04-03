a27ed91fa10a3a4a27b7fcd69760054ceb4e0acf
Grover101 <limachigrover09@gmail.com>
2023-04-02 22:13:30 -0400

    add routes endpoint users

---
+++ YogaIA-BackEnd/app/routes/user.routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+const UserController = require('../controllers/user.controller.js')
+
+router.get('/description', UserController.description)
+router.post('/', UserController.create)
+router.get('/:id', UserController.findOne)
+
+module.exports = router
