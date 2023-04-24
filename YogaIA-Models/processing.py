ea5933cb3f9e94f9953b5697b1d99b82aeee48a5
Grover101 <limachigrover09@gmail.com>
2023-04-24 10:36:47 -0400

    add: method detect image

--- YogaIA-Models/processing.py
+++ YogaIA-Models/processing.py
@@ -14,3 +14,11 @@ if ('movenet_thunder.tflite' not in os.listdir()):
         'https://tfhub.dev/google/lite-model/movenet/singlepose/thunder/tflite/float16/4?lite-format=tflite', 'movenet_thunder.tflite')
 
 movenet = Movenet('movenet_thunder')
+
+# Deteccion de una imagen y retorna una tupla
+def detect(input_tensor, inference_count=3):
+    movenet.detect(input_tensor.numpy(), reset_crop_region=True)
+    for _ in range(inference_count - 1):
+        detection = movenet.detect(
+            input_tensor.numpy(), reset_crop_region=False)
+    return detection
