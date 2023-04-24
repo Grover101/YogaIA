4abcefa81ac51af37941f1988eb4a177b1f37eeb
Grover101 <limachigrover09@gmail.com>
2023-04-24 11:29:39 -0400

    add: methods get points

--- YogaIA-Models/training.py
+++ YogaIA-Models/training.py
@@ -18,3 +18,48 @@ def load_csv(csv_path):
     y = keras.utils.to_categorical(y)
 
     return X, y, classes
+
+
+def get_center_point(landmarks, left_bodypart, right_bodypart):
+    """Calculates the center point of the two given landmarks."""
+    left = tf.gather(landmarks, left_bodypart.value, axis=1)
+    right = tf.gather(landmarks, right_bodypart.value, axis=1)
+    center = left * 0.5 + right * 0.5
+    return center
+
+
+def get_pose_size(landmarks, torso_size_multiplier=2.5):
+    """Calculates pose size.
+
+    It is the maximum of two values:
+    * Torso size multiplied by `torso_size_multiplier`
+    * Maximum distance from pose center to any pose landmark
+    """
+    # Hips center
+    hips_center = get_center_point(landmarks, BodyPart.LEFT_HIP,
+                                   BodyPart.RIGHT_HIP)
+
+    # Shoulders center
+    shoulders_center = get_center_point(landmarks, BodyPart.LEFT_SHOULDER,
+                                        BodyPart.RIGHT_SHOULDER)
+
+    # Torso size as the minimum body size
+    torso_size = tf.linalg.norm(shoulders_center - hips_center)
+    # Pose center
+    pose_center_new = get_center_point(landmarks, BodyPart.LEFT_HIP,
+                                       BodyPart.RIGHT_HIP)
+    pose_center_new = tf.expand_dims(pose_center_new, axis=1)
+    # Broadcast the pose center to the same size as the landmark vector to
+    # perform substraction
+    pose_center_new = tf.broadcast_to(pose_center_new,
+                                      [tf.size(landmarks) // (17*2), 17, 2])
+
+    # Dist to pose center
+    d = tf.gather(landmarks - pose_center_new, 0, axis=0,
+                  name="dist_to_pose_center")
+    # Max dist to pose center
+    max_dist = tf.reduce_max(tf.linalg.norm(d, axis=0))
+
+    # Normalize scale
+    pose_size = tf.maximum(torso_size * torso_size_multiplier, max_dist)
+    return pose_size
