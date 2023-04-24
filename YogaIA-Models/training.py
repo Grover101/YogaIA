3ac23e20b08d33f0e09bb4e8b5c3f93c11826882
Grover101 <limachigrover09@gmail.com>
2023-04-24 11:25:36 -0400

    add: method load csv

--- YogaIA-Models/training.py
+++ YogaIA-Models/training.py
@@ -5,3 +5,16 @@ from data import BodyPart
 import tensorflow as tf
 
 tfjs_model_dir = 'model'
+
+
+# loading final csv file
+def load_csv(csv_path):
+    df = pd.read_csv(csv_path)
+    df.drop(['filename'], axis=1, inplace=True)
+    classes = df.pop('class_name').unique()
+    y = df.pop('class_no')
+
+    X = df.astype('float64')
+    y = keras.utils.to_categorical(y)
+
+    return X, y, classes
