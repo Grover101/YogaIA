248af8437cd19facac760d71d9e81be904ab54bc
Grover101 <limachigrover09@gmail.com>
2023-04-24 11:30:43 -0400

    add: methods normalize, landmarks and preprocess data

--- YogaIA-Models/training.py
+++ YogaIA-Models/training.py
@@ -63,3 +63,45 @@ def get_pose_size(landmarks, torso_size_multiplier=2.5):
     # Normalize scale
     pose_size = tf.maximum(torso_size * torso_size_multiplier, max_dist)
     return pose_size
+
+
+def normalize_pose_landmarks(landmarks):
+    """Normalizes the landmarks translation by moving the pose center to (0,0) and
+    scaling it to a constant pose size.
+  """
+  # Move landmarks so that the pose center becomes (0,0)
+    pose_center = get_center_point(landmarks, BodyPart.LEFT_HIP,
+                                   BodyPart.RIGHT_HIP)
+
+    pose_center = tf.expand_dims(pose_center, axis=1)
+    # Broadcast the pose center to the same size as the landmark vector to perform
+    # substraction
+    pose_center = tf.broadcast_to(pose_center,
+                                  [tf.size(landmarks) // (17*2), 17, 2])
+    landmarks = landmarks - pose_center
+
+    # Scale the landmarks to a constant pose size
+    pose_size = get_pose_size(landmarks)
+    landmarks /= pose_size
+    return landmarks
+
+
+def landmarks_to_embedding(landmarks_and_scores):
+    """Converts the input landmarks into a pose embedding."""
+    # Reshape the flat input into a matrix with shape=(17, 3) // 17,2 sin score, con score 17,3
+    reshaped_inputs = keras.layers.Reshape((17, 3))(landmarks_and_scores)
+
+    # Normalize landmarks 2D
+    landmarks = normalize_pose_landmarks(reshaped_inputs[:, :, :2])
+    # Flatten the normalized landmark coordinates into a vector
+    embedding = keras.layers.Flatten()(landmarks)
+    return embedding
+
+
+def preprocess_data(X_train):
+    processed_X_train = []
+    for i in range(X_train.shape[0]):
+        embedding = landmarks_to_embedding(tf.reshape(
+            tf.convert_to_tensor(X_train.iloc[i]), (1, 51)))  # 1, 34  sin score, con score 51
+        processed_X_train.append(tf.reshape(embedding, (34)))
+    return tf.convert_to_tensor(processed_X_train)
