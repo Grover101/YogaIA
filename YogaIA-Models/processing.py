da246d7747d5040716d9d0d1fb6d8ea9476facca
Grover101 <limachigrover09@gmail.com>
2023-04-24 10:41:44 -0400

    add: class preprocessor

--- YogaIA-Models/processing.py
+++ YogaIA-Models/processing.py
@@ -22,3 +22,21 @@ def detect(input_tensor, inference_count=3):
         detection = movenet.detect(
             input_tensor.numpy(), reset_crop_region=False)
     return detection
+
+# Esta clase procesa las poses de las diferentes imagenes con puntos claves en coordenadas(x y)
+# Luego pasandolas a un archivo csv
+class Preprocessor(object):
+    def __init__(self, images_in_folder, csv_path, state=''):
+        self._images_in_folder = images_in_folder
+        self._csvs_out_path = csv_path
+        self._csvs_out_folder_per_class = 'temp'
+        self._message = []
+        self._state = state
+
+        if (self._csvs_out_folder_per_class not in os.listdir()):
+            os.makedirs(self._csvs_out_folder_per_class)
+
+        # obtner lista de clases de modelo
+        self._pose_class_names = sorted(
+            [n for n in os.listdir(images_in_folder)]
+        )
