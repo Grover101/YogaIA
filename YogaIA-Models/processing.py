673f39e7c201704411e1b2e676bb3967cfbf2a83
Grover101 <limachigrover09@gmail.com>
2023-04-24 10:48:54 -0400

    add: method landmarks as dataframe

--- YogaIA-Models/processing.py
+++ YogaIA-Models/processing.py
@@ -40,3 +40,41 @@ class Preprocessor(object):
         self._pose_class_names = sorted(
             [n for n in os.listdir(images_in_folder)]
         )
+
+    def all_landmarks_as_dataframe(self):
+        total_df = None
+        for index, class_name in enumerate(self._pose_class_names):
+            csv_out_path = os.path.join(
+                self._csvs_out_folder_per_class, class_name + '.csv')
+            per_class_df = pd.read_csv(csv_out_path, header=None)
+
+            # Agrega las etiquetas
+            per_class_df['class_no'] = [index]*len(per_class_df)
+            # per_class_df['class_name'] = [index]*len(per_class_df)
+            # print([class_name]*len(per_class_df))
+            per_class_df['class_name'] = [class_name]*len(per_class_df)
+
+            # Sera de forma temporal las carpetas
+            # Agregue el nombre de la carpeta a la primera columna del nombre del archivo
+            per_class_df[per_class_df.columns[0]] = class_name + \
+                '/' + per_class_df[per_class_df.columns[0]]
+
+            if total_df is None:
+                total_df = per_class_df
+            else:
+                total_df = pd.concat([total_df, per_class_df], axis=0)
+
+        list_name = [[bodypart.name + '_x', bodypart.name + '_y',
+                      bodypart.name + '_score'] for bodypart in BodyPart]
+
+        # Encabezado de cada columna
+        header_name = []
+        for columns_name in list_name:
+            header_name += columns_name
+        header_name = ['filename'] + header_name
+        header_map = {total_df.columns[i]: header_name[i]
+                      for i in range(len(header_name))}
+
+        total_df.rename(header_map, axis=1, inplace=True)
+
+        return total_df
