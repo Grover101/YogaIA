85b3af34e61a4143307cc88013269e7d6517b00f
Grover101 <limachigrover09@gmail.com>
2023-04-24 11:24:24 -0400

    add: libraries

---
+++ YogaIA-Models/training.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from tensorflow import keras
+from sklearn.model_selection import train_test_split
+from data import BodyPart
+import tensorflow as tf
+
+tfjs_model_dir = 'model'
