82478f3ec88f70a10f986f9992d3fb87a8244450
Grover101 <limachigrover09@gmail.com>
2023-04-24 10:17:08 -0400

    add: import model movenet

--- YogaIA-Models/processing.py
+++ YogaIA-Models/processing.py
@@ -7,3 +7,10 @@ import wget
 import csv
 import tqdm
 from data import BodyPart
+
+# Descarga de modelo movenet para la deteccion de postura de una persona
+if ('movenet_thunder.tflite' not in os.listdir()):
+    wget.download(
+        'https://tfhub.dev/google/lite-model/movenet/singlepose/thunder/tflite/float16/4?lite-format=tflite', 'movenet_thunder.tflite')
+
+movenet = Movenet('movenet_thunder')
